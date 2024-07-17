892a241bab33991c9ede7899b3ff0b0764e32e60
jondoescoding <jondidathing.jm@gmail.com>
2024-07-16 21:47:48 -0500

    updated tools

--- kryptt/AI/app.py
+++ kryptt/AI/app.py
@@ -6,14 +6,17 @@ from tools import *
 from langchain.agents import AgentExecutor, create_tool_calling_agent
 from langchain_core.prompts import ChatPromptTemplate
 
-
-tools = [python_assistant_tool]
+tools = [
+            python_assistant_tool, 
+            coin_gecko_fetch_ohlc_tool,
+            coin_gecko_fetch_token_tool
+        ]
 
 prompt = ChatPromptTemplate.from_messages(
     [
         (
             "system",
-            "You are a helpful crypto assistant with access to a wide range of tools to help answer questions about cryptocurrencies, conduct data analysis, research and code.",
+            "You are to act as a helpful cryptocurrency assistant. You have access to python. Python is good for general questions, data analysis and machine learning. In addition you have the capabilities to conduct trade.",
         ),
         ("placeholder", "{chat_history}"),
         ("human", "{input}"),
@@ -21,12 +24,14 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-agent = create_tool_calling_agent(LLM.gpt3_5, tools, prompt)
+agent = create_tool_calling_agent(LLM.gorilla, tools, prompt)
 
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, handle_parsing_errors=True)
 
-print(agent_executor.invoke(
-    {
-        "input": "use python to show me: what is the current time?"
-    })["output"]
-    )
+print(
+    agent_executor.invoke(
+        {
+            "input": "Fetch the OHLC data of the curve token data from the last 7 days. Use machine learning to predict the price for the next 2 days using the fetched data."
+        }
+    )['output']
+)
