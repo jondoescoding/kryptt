9eb2970835f28ff3c92280f8de28319820bea0c1
jondoescoding <jondidathing.jm@gmail.com>
2024-07-17 00:44:10 -0500

    added new python tool and requisite imports

--- kryptt/AI/tools.py
+++ kryptt/AI/tools.py
@@ -1,10 +1,38 @@
-# Local 
-from config import OPENAI_TOKEN, OPENROUTER_TOKEN, GROQ_API_KEY
-from validation import coinGeckoFetchTokenInput, coinGeckoFetchOHLCInput, coinGeckoFetchTokenDataInput
+# Local
+from config import GROQ_API_KEY
+from validation import coinGeckoFetchTokenInput, coinGeckoFetchOHLCInput, coinGeckoFetchTokenDataInput, MoneyInput, AIInput
 from coin_gecko_tools import fetch_token, fetch_ohlc_by_id, fetch_coin_data
+from forex_tools import convert_coin_price
 
 # Langchain
 from langchain.tools import StructuredTool
+
+# Phidata
+from phi.assistant.python import PythonAssistant
+from phi.llm.groq import Groq
+
+# Python Tool
+python_assistant = PythonAssistant(
+    llm=Groq(model="llama3-70b-8192", api_key=GROQ_API_KEY),
+    pip_install=True,
+    show_function_calls=True,
+    run_code=True
+)
+
+python_assistant_tool = StructuredTool.from_function(
+    func=python_assistant.print_response,
+    description="A Python shell for data analysis, predictions, converting natural language dates to UNIX timestamps and machine learning. Use this to execute python commands. Input should be a valid python command.",
+    name="python_assistant_tool",
+    args_schema=AIInput
+)
+
+# This tool converts the given USD coin price to the specified currency using a conversion API.
+convert_coin_price_tool = StructuredTool.from_function(
+    func=convert_coin_price,
+    name="Convert_Coin_Price",
+    description="This converts the given USD coin price to the specified currency using a conversion API.",
+    args_schema=MoneyInput
+)
 
 # Coingecko tools
 coin_gecko_fetch_token_tool = StructuredTool.from_function(
